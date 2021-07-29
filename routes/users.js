9f882ce5c0369bc5c3f180d6ce0a2efeac473803
Jaime Alvarez <jrralvarez@gmail.com>
2021-07-28 21:38:00 -0400

    able to add a new user

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,25 @@ router.get("/", async (req, res) => {
     }
 });
 
+//register new user
+router.post("/", async (req, res) => {
+    try {
+        let user = await User.findOne({email:req.body.email});
+        if (user) return res.status(400).send("User already registered");
+
+        user = new User ({
+            userName: req.body.userName,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        await user.save();
+        
+        return res
+        .send(user);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
 module.exports = router;
