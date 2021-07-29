56c75be99dd9f4beb854ee7341187d5e7c25879c
Jaime Alvarez <jrralvarez@gmail.com>
2021-07-28 21:59:32 -0400

    added user validation

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-const { User } = require("../models/user");
+const { User, validateUser } = require("../models/user");
 const express = require("express");
 const router = express.Router();
 
@@ -15,6 +15,9 @@ router.get("/", async (req, res) => {
 //register new user
 router.post("/", async (req, res) => {
     try {
+        const {error} = validateUser(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+
         let user = await User.findOne({email:req.body.email});
         if (user) return res.status(400).send("User already registered");
 
