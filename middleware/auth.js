b4b75d1a702be5f6b2918841e7ff9cbcbf0f9cc7
Ryan Huddleston <wryanhuddleston@gmail.com>
2021-08-02 13:37:08 -0400

    added env.TOKEN

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-var jwtSecret = process.env.JWT_SECRET;
+var jwtSecret = process.env.TOKEN;
 
 function auth(req, res, next) {
     const token = req.header("x-auth-token");
